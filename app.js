b85f68cc46f6d52c7a0722b5e82d9687b367491e
Shubham <20je0931@me.iitism.ac.in>
2022-08-18 23:48:55 +0530

    update app.js

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({                       // Google Strategy
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
-    // callbackURL: "https://radiant-island-56857.herokuapp.com/auth/google/secrets"
+    // callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://radiant-island-56857.herokuapp.com/auth/google/secrets"
     // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
